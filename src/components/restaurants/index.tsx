996db1cbf958b2de8c85e2fce24dcc04d54a41bd
Camile Oliveira <camileoliv.s.j@gmail.com>
2025-05-15 16:23:01 -0300

    Primeiro commit

---
+++ src/components/restaurants/index.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import { FlatList } from 'react-native'
+import { RestaurantItem } from './horizontal'
+
+export interface RestaurantsProps{
+    id: string;
+    name: string;
+    image: string;
+}
+
+export function Restaurants() {
+    const [restaurants, setRestaurants] = useState<RestaurantsProps[]>([])
+
+    useEffect(() => {
+        async function getFoods(){
+            const response = await fetch("http://192.168.100.23:3000/restaurants")
+            const data = await response.json()
+            setRestaurants(data);
+        }
+        getFoods();
+    }, [])
+    
+  return (
+    <FlatList
+        data={restaurants}
+        renderItem={ ({item}) => <RestaurantItem item={item}/>}
+        horizontal={true}
+        contentContainerStyle={{ gap: 14, paddingLeft: 16, paddingRight: 16}}
+        showsHorizontalScrollIndicator={false}
+    />
+  )
+}
